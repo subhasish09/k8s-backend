f408e09ada4b3669c7ac479cbf7492490346207b
subhasish09 <107799425+subhasish09@users.noreply.github.com>
2023-09-09 05:24:32 +0000

    flask2:0.2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/send', methods=['POST'])
 def send_message():
     message = request.form.get('message', '')
-    return jsonify({"response": f"Hello from Backend: {message}"})
+    return jsonify({"response": f"welcome from Backend: {message}"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
