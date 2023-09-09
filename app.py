da9cc608c9f64ad733e1bbcd13d53a7e27f3b354
subhasish09 <107799425+subhasish09@users.noreply.github.com>
2023-09-09 04:17:36 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/send', methods=['POST'])
+def send_message():
+    message = request.form.get('message', '')
+    return jsonify({"response": f"Hello from Backend: {message}"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6000)
